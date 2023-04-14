30ad460871a0c228d09e199f9ab033b3e6fb37f0
GitHub Action <action@github.com>
2023-04-14 08:18:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/warmlix/codescan/services/MyProjectService.kt
+++ src/main/kotlin/com/github/warmlix/codescan/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.warmlix.codescan.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.warmlix.codescan.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
